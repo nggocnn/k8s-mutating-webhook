1fc874b1e9135a3b4fe17c7f3142a49a19000788
nggocnn <nhungocnnn2509@gmail.com>
2024-08-08 10:02:45 +0700

    full

--- namespace-webhook/main.go
+++ namespace-webhook/main.go
@@ -33,7 +33,6 @@ func main() {
 
 func ServerNamespaceBackup(w http.ResponseWriter, r *http.Request) {
 	logger := logrus.WithFields(logrus.Fields{"uri": r.RequestURI})
-	logger.Debug("Received mutation request")
 
 	admissionReview, err := parseRequest(*r)
 	if err != nil {
@@ -44,17 +43,22 @@ func ServerNamespaceBackup(w http.ResponseWriter, r *http.Request) {
 
 	namespace := corev1.Namespace{}
     oldNamespace := corev1.Namespace{}
-	err = json.Unmarshal(admissionReview.Request.Object.Raw, &namespace)
-    if err != nil {
-		logger.WithFields(logrus.Fields{"error": err}).Error("Failed to parse namespace")
-		http.Error(w, fmt.Sprintf("Could not parse namespace: %v", err), http.StatusBadRequest)
-		return
-	}
-
     switch admissionReview.Request.Operation {
         case admissionv1.Create:
+            err := json.Unmarshal(admissionReview.Request.Object.Raw, &namespace)
+            if err != nil {
+                logger.WithFields(logrus.Fields{"error": err}).Error("Failed to parse namespace")
+                http.Error(w, fmt.Sprintf("Could not parse namespace: %v", err), http.StatusBadRequest)
+                return
+            }
 			logger.Info(fmt.Sprintf("Namespace %s created", namespace.Name))
 		case admissionv1.Update:
+            err := json.Unmarshal(admissionReview.Request.Object.Raw, &namespace)
+            if err != nil {
+                logger.WithFields(logrus.Fields{"error": err}).Error("Failed to parse namespace")
+                http.Error(w, fmt.Sprintf("Could not parse namespace: %v", err), http.StatusBadRequest)
+                return
+            }
 			logger.Info(fmt.Sprintf("Namespace %s updated", namespace.Name))
             err = json.Unmarshal(admissionReview.Request.OldObject.Raw, &oldNamespace)
             if err != nil {
@@ -63,13 +67,13 @@ func ServerNamespaceBackup(w http.ResponseWriter, r *http.Request) {
                 return
             }
 		case admissionv1.Delete:
-			logger.Info(fmt.Sprintf("Namespace %s deleted", namespace.Name))
             err = json.Unmarshal(admissionReview.Request.OldObject.Raw, &oldNamespace)
             if err != nil {
                 logger.WithFields(logrus.Fields{"error": err}).Error("Failed to parse old namespace")
                 http.Error(w, fmt.Sprintf("Could not parse old namespace: %v", err), http.StatusBadRequest)
                 return
             }
+            logger.Info(fmt.Sprintf("Namespace %s deleted", oldNamespace.Name))
 		default:
             logger.Info("Unknown operation")
 			return
@@ -97,54 +101,25 @@ func ServerNamespaceBackup(w http.ResponseWriter, r *http.Request) {
         case admissionv1.Create:
             if targetKey && targetName != "" && runtimeKey && runtime == "target" {
                 cronExpression := "@every 5m"
-                err := createVeleroSchedule(*r, dynamicClient, projectName, targetName, namespace.Name, cronExpression, logger)
-                if err != nil {
-                    logger.WithFields(logrus.Fields{"error": err}).Error("Failed to create Velero schedule")
-                    http.Error(w, fmt.Sprintf("Failed to create Velero schedule: %v", err), http.StatusInternalServerError)
-                    return
-                }
-                err = createVeleroBackup(*r, dynamicClient,  projectName, targetName, namespace.Name, logger)
-                if err != nil {
-                    logger.WithFields(logrus.Fields{"error": err}).Error("Failed to create Velero backup")
-                    http.Error(w, fmt.Sprintf("Failed to create Velero backup: %v", err), http.StatusInternalServerError)
-                    return
-                }
+                createVeleroSchedule(*r, dynamicClient, projectName, targetName, namespace.Name, cronExpression, logger)
+                createVeleroBackup(*r, dynamicClient,  projectName, targetName, namespace.Name, logger)
             }  
         case admissionv1.Update:
             oldTargetName, oldTargetKey := oldNamespace.Labels["namespace.oam.dev/target"]
             oldRuntime, oldRuntimeKey := oldNamespace.Labels["usage.oam.dev/runtime"]
-            if targetKey && targetName != "" && runtimeKey && runtime == "target" && (!oldTargetKey || oldTargetName == "")  && (!oldRuntimeKey || oldRuntime == "") {
+            if targetKey && targetName != "" && runtimeKey && runtime == "target" && (!oldTargetKey || oldTargetName == "" || !oldRuntimeKey || oldRuntime == "") {
                 cronExpression := "@every 5m"
-                err := createVeleroSchedule(*r, dynamicClient, projectName, targetName, namespace.Name, cronExpression, logger)
-                if err != nil {
-                    logger.WithFields(logrus.Fields{"error": err}).Error("Failed to create Velero schedule")
-                    http.Error(w, fmt.Sprintf("Failed to create Velero schedule: %v", err), http.StatusInternalServerError)
-                    return
-                }
-                err = createVeleroBackup(*r, dynamicClient,  projectName, targetName, namespace.Name, logger)
-                if err != nil {
-                    logger.WithFields(logrus.Fields{"error": err}).Error("Failed to create Velero backup")
-                    http.Error(w, fmt.Sprintf("Failed to create Velero backup: %v", err), http.StatusInternalServerError)
-                    return
-                }
+                createVeleroSchedule(*r, dynamicClient, projectName, targetName, namespace.Name, cronExpression, logger)
+                createVeleroBackup(*r, dynamicClient,  projectName, targetName, namespace.Name, logger)
             } else if (!targetKey || targetName == "" || !runtimeKey || runtime != "target") && oldTargetKey && oldTargetName != "" && oldRuntimeKey && oldRuntime == "target" {
-                err := deleteVeleroSchedule(*r, dynamicClient, projectName, targetName, namespace.Name, logger)
-                if err != nil {
-                    logger.WithFields(logrus.Fields{"error": err}).Error("Failed to delete Velero schedule")
-                    http.Error(w, fmt.Sprintf("Failed to delete Velero schedule: %v", err), http.StatusInternalServerError)
-                    return
-                }
+                deleteVeleroSchedule(*r, dynamicClient, projectName, oldTargetName, namespace.Name, logger)
             }
             
-            
         case admissionv1.Delete:
-            if targetKey && targetName != "" && runtimeKey && runtime == "target" {
-                err := deleteVeleroSchedule(*r, dynamicClient, projectName, targetName, namespace.Name, logger)
-                if err != nil {
-                    logger.WithFields(logrus.Fields{"error": err}).Error("Failed to delete Velero schedule")
-                    http.Error(w, fmt.Sprintf("Failed to delete Velero schedule: %v", err), http.StatusInternalServerError)
-                    return
-                }
+            oldTargetName, oldTargetKey := oldNamespace.Labels["namespace.oam.dev/target"]
+            oldRuntime, oldRuntimeKey := oldNamespace.Labels["usage.oam.dev/runtime"]
+            if oldTargetKey && oldTargetName != "" && oldRuntimeKey && oldRuntime == "target" {
+                deleteVeleroSchedule(*r, dynamicClient, projectName, oldTargetName, namespace.Name, logger)
             }
     }
 
@@ -166,7 +141,7 @@ func ServerNamespaceBackup(w http.ResponseWriter, r *http.Request) {
 	w.Write(respBytes)
 }
 
-func createVeleroSchedule(r http.Request, client dynamic.Interface, projectName string, targetName string, namespaceName string, cronExpression string, logger *logrus.Entry) error {
+func createVeleroSchedule(r http.Request, client dynamic.Interface, projectName string, targetName string, namespaceName string, cronExpression string, logger *logrus.Entry) {
 	scheduleName := fmt.Sprintf("%s-%s-%s", projectName, targetName, namespaceName)
     
     veleroScheduleResource := schema.GroupVersionResource{
@@ -177,8 +152,7 @@ func createVeleroSchedule(r http.Request, client dynamic.Interface, projectName 
 
 	_, err := client.Resource(veleroScheduleResource).Namespace("velero").Get(r.Context(), scheduleName, metav1.GetOptions{})
 	if err != nil {
-		logger.Info(fmt.Sprintf("Velero schedule %s already exists", scheduleName))
-		return nil
+        logger.Info(fmt.Sprintf("%v", err))
 	}
 
 	veleroSchedule := &unstructured.Unstructured{
@@ -206,13 +180,11 @@ func createVeleroSchedule(r http.Request, client dynamic.Interface, projectName 
 	logger.Info(fmt.Sprintf("Creating Velero schedule %s", scheduleName))
 	_, err = client.Resource(veleroScheduleResource).Namespace("velero").Create(r.Context(), veleroSchedule, metav1.CreateOptions{})
 	if err != nil {
-		return err
+        logger.Info(fmt.Sprintf("%v", err))
 	}
-
-	return nil
 }
 
-func createVeleroBackup(r http.Request, client dynamic.Interface, projectName string, targetName string, namespaceName string, logger *logrus.Entry) error {
+func createVeleroBackup(r http.Request, client dynamic.Interface, projectName string, targetName string, namespaceName string, logger *logrus.Entry) {
 	scheduleName := fmt.Sprintf("%s-%s-%s", projectName, targetName, namespaceName)
     backupName := fmt.Sprintf("%s-%s", scheduleName, time.Now().Format("20060102150405"))
 
@@ -244,13 +216,11 @@ func createVeleroBackup(r http.Request, client dynamic.Interface, projectName st
 	logger.Info(fmt.Sprintf("Creating Velero backup %s", backupName))
 	_, err := client.Resource(veleroBackupResource).Namespace("velero").Create(r.Context(), veleroBackup, metav1.CreateOptions{})
 	if err != nil {
-		return err
+        logger.Error(fmt.Sprintf("%v", err))
 	}
-
-    return nil
 }
 
-func deleteVeleroSchedule(r http.Request, client dynamic.Interface, projectName string, targetName string, namespaceName string, logger *logrus.Entry) error {
+func deleteVeleroSchedule(r http.Request, client dynamic.Interface, projectName string, targetName string, namespaceName string, logger *logrus.Entry) {
     scheduleName := fmt.Sprintf("%s-%s-%s", projectName, targetName, namespaceName)
 
 	veleroScheduleResource := schema.GroupVersionResource{
@@ -262,10 +232,8 @@ func deleteVeleroSchedule(r http.Request, client dynamic.Interface, projectName 
 	logger.Info(fmt.Sprintf("Deleting Velero schedule %s", scheduleName))
 	err := client.Resource(veleroScheduleResource).Namespace("velero").Delete(r.Context(), scheduleName, metav1.DeleteOptions{})
 	if err != nil {
-		return err
+        logger.Error(fmt.Sprintf("%v", err))
 	}
-
-	return nil
 }
 
 func ServerHealth(w http.ResponseWriter, r *http.Request) {
@@ -293,9 +261,7 @@ func setLogger() {
 }
 
 func parseRequest(r http.Request) (*admissionv1.AdmissionReview, error) {
-	logrus.Debug("Parsing request")
-
-	if r.Header.Get("Content-Type") != "application/json" {
+    if r.Header.Get("Content-Type") != "application/json" {
 		return nil, fmt.Errorf("Content-Type: %q should be %q", r.Header.Get("Content-Type"), "application/json")
 	}
 
